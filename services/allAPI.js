967971dc4d423f5de07d92e7af263d5d38c64ed6
Akshay-K-git <akshay2103k@gmail.com>
2024-12-03 23:47:40 +0530

    updations

--- services/allAPI.js
+++ services/allAPI.js
@@ -8,3 +8,6 @@ export const registerAPI= async(reqbody)=>{
 export const loginAPI= async(reqbody)=>{
     return await commonAPI('post',`${ServerURL}/api/login`,reqbody,"")
 }
+export const addProjectAPI= async(reqbody,reqHeader)=>{
+    return await commonAPI('post',`${ServerURL}/api/login`,reqbody,reqHeader)
+}
--- src/Components/Addproject.jsx
+++ src/Components/Addproject.jsx
@@ -1,15 +1,58 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import { FaFileUpload } from "react-icons/fa";
+import { addProjectAPI } from '../../services/allAPI';
 function Addproject() {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const[priview,setpreview]=useState("")
+
+  const[projectDetails,setprojectDetails]=useState({
+    title:"",
+    language:"",
+    github:"",
+    website:"",
+    overview:"",
+    projectimg:""
+  })
+  const handleAddproject=async()=>{
+console.log(projectDetails);
+const{title,language,github,website,overview,projectimg}=projectDetails
+if(!title||!language||!github||!website||!overview||!projectimg){
+  alert("Please fill the form")
+}
+else{
+  const reqBody = new FormData()
+  reqBody.append("title",title)
+  reqBody.append("language",language)
+  reqBody.append("github",github)
+  reqBody.append("website",website)
+  reqBody.append("overview",overview)
+  reqBody.append("projectimg",projectimg)
+
+  try {
+   
+const response = await addProjectAPI(reqBody)
+    
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
+
+  }
+  useEffect(()=>{
+    if(projectDetails.projectimg){
+    setpreview(URL.createObjectURL(projectDetails.projectimg))
+    }
+  },[projectDetails.projectimg])
+
 
   return (
     <>
@@ -31,25 +74,26 @@ function Addproject() {
         <div className="row">
           <div className="col-6 d-flex align-items-center">
             <label>
-              <input type="file" style={{display:'none'}}/>
-              <img src="./addproject.png" width={'380px'} height={'270px'} alt="" />
+              <input type="file"onChange={e=>setprojectDetails({...projectDetails,projectimg:e.target.files[0]})} style={{display:'none'}}/>
+              <img src={priview?priview:"./addproject.png"} width={'380px'} height={'270px'} alt="" />
             </label>
           </div>
           <div className="col-6">
           <FloatingLabel controlId="floatingInput"label="Title"className="mb-3">
-        <Form.Control type="text" placeholder="" />
+        <Form.Control onChange={e=>setprojectDetails({...projectDetails,title:e.target.value})} type="text" placeholder="" />
       </FloatingLabel>
       <FloatingLabel controlId="floatingInput"label="Language"className="mb-3">
-        <Form.Control type="text" placeholder="" />
+        <Form.Control onChange={e=>setprojectDetails({...projectDetails,language:e.target.value})} type="text" placeholder="" />
       </FloatingLabel>
       <FloatingLabel controlId="floatingInput"label="Github"className="mb-3">
-        <Form.Control type="text" placeholder="" />
+        <Form.Control onChange={e=>setprojectDetails({...projectDetails,github:e.target.value})} type="text" placeholder="" />
       </FloatingLabel>
       <FloatingLabel controlId="floatingInput"label="Website"className="mb-3">
-        <Form.Control type="text" placeholder="" />
+        <Form.Control onChange={e=>setprojectDetails({...projectDetails,website:e.target.value})} type="text" placeholder="" />
       </FloatingLabel>
       <FloatingLabel controlId="floatingTextarea2" label="Overview">
         <Form.Control
+        onChange={e=>setprojectDetails({...projectDetails,overview:e.target.value})}
           as="textarea"
           placeholder="Leave a comment here"
           style={{ height: '100px' }}
@@ -64,7 +108,7 @@ function Addproject() {
           <Button variant="secondary" onClick={handleClose}>
             cancel
           </Button>
-          <Button variant="primary"><FaFileUpload className='mb-2 me-1' />Upload</Button>
+          <Button variant="primary" onClick={handleAddproject}><FaFileUpload className='mb-2 me-1' />Upload</Button>
         </Modal.Footer>
       </Modal>
     </>
